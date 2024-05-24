497c3d566bf0b8864d7942af2f07a013dfd1f5e3
ClementMadiot <clementmadiot09@gmail.com>
2024-05-24 11:46:42 +0200

    header

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import Navbar from './Navbar';
+import BannerBackground from "../assets/images/home-banner-background.png"
+import { FiArrowRight } from 'react-icons/fi';
 
 const Home = () => {
   return (
     <div className='home-container'>
       <Navbar/>
+      <header className="home-banner-container">
+        <div className="home-bannerImage-container">
+          <img src={BannerBackground} alt="banner-background" />
+        </div>
+        <div className="home-text-section">
+          <h1 className="primary-heading">
+            Your Favorite Food Delivered Hot & Fresh
+          </h1>
+          <p className='primary-text'>Healthy switcher chefs do all the prep work, like peeding, chopping & marinating, so you can cook a fresh food.</p>
+          <button className="secondary-button">
+            Order Now <FiArrowRight/>
+          </button>
+        </div>
+      </header>
     </div>
   );
 };
